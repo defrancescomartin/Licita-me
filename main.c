a3675f13d008010c4b0bce1cc3e407e85e68da84
MauriPercovich <mauriciopercovich22@gmail.com>
2022-10-12 00:28:23 +0000

    finished RUT validation checker

--- main.c
+++ main.c
@@ -1,46 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+/**
+* Check_Rut - Function that checks if a RUT code given is valid through DV.
+* @rut: rut code given
+* Return: 0 if valid, -1 otherwise
+**/
 int Check_Rut(char rut[])
 {
 char ver[12] = "43298765432";
 int dv, dc, total = 0, x = 0;
 int val_a = 0, val_b = 0;
 
-        if (strlen(rut) != 12)
-        {
-            return (-1);
-        }
-        dc = rut[11] - '0';
+	if (strlen(rut) != 12)
+	{
+		printf("RUT code should be 12 digits.\n");
+		return (-1);
+	}
+	dc = rut[11] - '0';
 
-        while (x <= 10)
-        {
-			val_a = rut[x] - '0';
-			val_b = ver[x] - '0';
-            total += (val_a * val_b);
-		 	printf("total %d is: %d\n", x, total);
-            x++;
-        }
-	    printf("total is %d\n", total);
-        dv = 11 - (total % 11);
-        if (dv == 11)
-        {
-                dv = 0;
-        }
-        else if(dv == 10)
-        {
-                dv = 1;
-        }
-        if(dv == dc)
-        {
-                return (0);
-        }
-        else
-        {
-		printf("dv = %d\n", dv);
-		printf("dc = %d\n", dc);
-                return (-1);
-        }
+	while (x <= 10)
+	{
+		val_a = rut[x] - '0';
+		val_b = ver[x] - '0';
+		total += (val_a * val_b);
+		x++;
+	}
+	dv = 11 - (total % 11);
+	if (dv == 11)
+	{
+		dv = 0;
+	}
+	else if (dv == 10)
+	{
+		dv = 1;
+	}
+	if (dv == dc)
+	{
+		return (0);
+	}
+	else
+	{
+		return (-1);
+	}
 }
 /**
  * main - check the code
@@ -56,12 +58,12 @@ int a;
 
 	if (a == 0)
 	{
-		printf("Sexo\n");
+		printf("Valid RUT\n");
 		return (0);
 	}
-	else if (a == -1);
+	else if (a == -1)
 	{
-		printf("No Sexo\n");
-		return (1);
+		printf("Invalid RUT\n");
+		return (-1);
 	}
 }
