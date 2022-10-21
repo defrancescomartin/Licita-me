fb31eeeefd3886fd44c7fc63be94edb1b1138f86
SFreire2022 <sebastiancazenave@gmail.com>
2022-10-21 13:14:14 -0300

    Change static folder for flask

--- Controller/licitame.py
+++ Controller/licitame.py
@@ -12,7 +12,7 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
 
 
-app = Flask(__name__)
+app=Flask(__name__, template_folder='../View', static_folder='../View')
 # Config connection from licitame app file to LicitaMeDB database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://licitame:Password-2022@localhost/LicitaMeDB'
 # Turn off this warning
@@ -24,57 +24,53 @@ db  = SQLAlchemy(app)
 # Use bcrypt so i can hash the passwords
 bcrypt = Bcrypt(app)
 
-
-# This part of login manager will allow me
-# handle things on login
+# This part of login manager will allow handle things on login
 # Loading users from credentials
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'signin'
 
-app=Flask(__name__, template_folder='../View')
-
 @app.route('/', strict_slashes=False)
 def index():
     return render_template('landing_page.html')
 
-@app.route('/signin')
+@app.route('/signin', strict_slashes=False)
 def signin():
     return render_template('sign_in.html')
 
-@app.route('/signup')
+@app.route('/signup', strict_slashes=False)
 def signup():
         return render_template('sign_up.html')
 
-@app.route('/home')
+@app.route('/home', strict_slashes=False)
 @login_required
 def home():
     return render_template ('home.html')
 
-@app.route('/signup')
+@app.route('/signup', strict_slashes=False)
 def sign_up1():
     return render_template ('sign_up.html')
 
-@app.route('/signup2')
+@app.route('/signup2', strict_slashes=False)
 def sign_up2():
     return render_template ('signup2.html')
 
-@app.route('/signup3')
+@app.route('/signup3', strict_slashes=False)
 def sign_up3():
     return render_template ('signup3.html')
 
-@app.route('/editclient')
+@app.route('/editclient', strict_slashes=False)
 @login_required
 def edit_client():
     return 'edit client'
 
-@app.route('/createrequest')
+@app.route('/createrequest', strict_slashes=False)
 @login_required
 def create_request():
 	return 'Create request'
     #return render_template ('request.html')
 
-@app.route('/createbid')
+@app.route('/createbid', strict_slashes=False)
 @login_required
 def create_bid():
 	return 'Create bid'
