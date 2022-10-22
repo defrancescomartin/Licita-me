da697c8cf2294f9b5fa47d7a0f30212bdd48fb13
SFreire2022 <sebastiancazenave@gmail.com>
2022-10-21 21:30:52 -0300

    Add Class definitions to interact with db

--- Controller/licitame.py
+++ Controller/licitame.py
@@ -26,15 +26,117 @@ bcrypt = Bcrypt(app)
 
 # This part of login manager will allow handle things on login
 # Loading users from credentials
-#login_manager = LoginManager()
-#login_manager.init_app(app)
-#login_manager.login_view = 'signin'
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'signin'
+
+class Company(db.Model,UserMixin):
+    # The id column is the Company's identity column
+    CompanyId   = db.Column(db.Integer, primary_key=True)
+    CompanyName = db.Column(db.String(20), nullable=False, unique=True)
+    RUT         = db.Column(db.Integer, nullable=False, unique=True)
+	RSocial     = db.Column(db.String(45), nullable=False)
+
+class User(db.Model,UserMixin):
+    # The id column is the user's identity column
+    UserId       = db.Column(db.Integer, primary_key=True)
+    CustomerName = db.Column(db.String(45), nullable=False, unique=True)
+    Password     = db.Column(db.String(80), nullable=False)
+	CompanyId    = db.Column(db.Integer, foreign_key=True)
+	Email        = db.Column(db.String(45), nullable=False, unique=True)
+
+class RegisterForm(FlaskForm):
+	#Company
+    CompanyName = StringField(validators=[
+                           InputRequired(),
+                           Length(min=4, max=45)],
+                           render_kw={"placeholder": "CompanyName"})
+    RUT = PasswordField(validators=[
+                             InputRequired(),
+                             Length(min=12, max=12)],
+                             render_kw={"placeholder": "RUT"})
+    RSocial = StringField(validators=[
+                           InputRequired(),
+                           Length(min=4, max=45)],
+                           render_kw={"placeholder": "RSocial"})
+	CompanyAddress = StringField(validators=[
+                           InputRequired(),
+                           Length(min=4, max=45)],
+                           render_kw={"placeholder": "CompanyAddress"})
+	CompanyState = StringField(validators=[
+                           InputRequired(),
+                           Length(min=4, max=45)],
+                           render_kw={"placeholder": "CompanyState"})
+	CompanyCity = StringField(validators=[
+                           InputRequired(),
+                           Length(min=4, max=45)],
+                           render_kw={"placeholder": "CompanyCity"})
+	CompanyPhone = StringField(validators=[
+                           InputRequired(),
+                           Length(min=4, max=20)],
+                           render_kw={"placeholder": "CompanyPhone"})
+	#User
+    CustomerName = StringField(validators=[
+                           InputRequired(),
+                           Length(min=4, max=45)],
+                           render_kw={"placeholder": "CustomerName"})
+    Phone = StringField(validators=[
+                           InputRequired(),
+                           Length(min=4, max=20)],
+                           render_kw={"placeholder": "Phone"})
+    Password = PasswordField(validators=[
+                             InputRequired(),
+                             Length(min=8, max=20)],
+                             render_kw={"placeholder": "Password"})
+    Email = StringField(validators=[
+                           InputRequired(),
+                           Length(min=4, max=45)],
+                           render_kw={"placeholder": "Email"})
+    Birthdate = StringField(validators=[
+                           InputRequired(),
+                           Length(min=4, max=45)],
+                           render_kw={"placeholder": "Birthdate"})
+    Address = StringField(validators=[
+                           InputRequired(),
+                           Length(min=4, max=45)],
+                           render_kw={"placeholder": "Address"})
+	State = StringField(validators=[
+                           InputRequired(),
+                           Length(min=4, max=45)],
+                           render_kw={"placeholder": "State"})
+	City = StringField(validators=[
+                           InputRequired(),
+                           Length(min=4, max=45)],
+                           render_kw={"placeholder": "City"})
+    submit = SubmitField('Register')
+
+def validate_username(self, RUT):
+    existing_user_username = User.query.filter_by(RUT=CustomerName.data).first()
+    if existing_user_username:
+        raise ValidationError('That username already exists. Please choose a different one.')
+
+# The username must be unique, i.e. each name
+# User must be different
+def validate_username(self, CustomerName):
+    existing_user_username = User.query.filter_by(CustomerName=CustomerName.data).first()
+    if existing_user_username:
+        raise ValidationError('That username already exists. Please choose a different one.')
+
+class LoginForm(FlaskForm):
+    CustomerName = StringField(validators=[InputRequired(),
+                                     Length(min=4, max=45)],
+                                     render_kw={"placeholder": "CustomerName"})
+    Password = PasswordField(validators=[InputRequired(),
+                                     Length(min=8, max=20)],
+                                     render_kw={"placeholder": "Password"})
+    submit = SubmitField('signin')
+
 
 @app.route('/', strict_slashes=False)
 def index():
     return render_template('landing_page.html')
 
-@app.route('/signin', strict_slashes=False)
+@app.route('/signin',  strict_slashes=False)
 def signin():
     return render_template('sign_in.html')
 
