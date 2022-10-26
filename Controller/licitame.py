4ae73058bebf47f9e7170ad7ff7ce6aa5f0a5c53
SFreire2022 <sebastiancazenave@gmail.com>
2022-10-25 23:20:29 -0300

    test

--- Controller/licitame.py
+++ Controller/licitame.py
@@ -309,12 +309,12 @@ def create_request():
 @login_required
 def request_by(request_id):
         # View Request by ID
-        request.args.get('request_id')
-        req = Request.query.filter_by(RequestId=request_id).first()
-        if req.id == current_user.id:
-            bids = Bid.query.filter_by(RequestId=request_id).all()
-            return render_template('my_request.html', req=req, bids=bids)
-        return render_template('request_inside.html', req=req)
+        if request_id:
+            req = Request.query.filter_by(RequestId=request_id).first()
+            if req.id == current_user.id:
+                bids = Bid.query.filter_by(RequestId=request_id).all()
+                return render_template('my_request.html', req=req, bids=bids)
+            return render_template('request_inside.html', req=req)
 
 @app.route('/create_bid/<int:request_id>', methods=['GET', 'POST'], strict_slashes=False)
 @login_required
