69dff72960e7e75281859864b4c1dd2785451d7c
SFreire2022 <sebastiancazenave@gmail.com>
2022-10-26 12:07:43 -0300

    Import

--- Controller/licitame.py
+++ Controller/licitame.py
@@ -8,7 +8,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user,  current_user
 from flask_bcrypt import Bcrypt
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, FileField
 from wtforms.validators import InputRequired, Length, ValidationError, EqualTo
 from datetime import datetime
 
